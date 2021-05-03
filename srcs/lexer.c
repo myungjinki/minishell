c17c99a55512d917e26625750c1783ea2d27f9c7
mki <mki@student.42seoul.kr>
2021-05-03 10:49:52 +0900

    lexer

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,13 +6,27 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 10:12:47 by mki               #+#    #+#             */
-/*   Updated: 2021/05/03 10:18:25 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/03 10:49:48 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_list	*lexer(char *s)
+int		chech_symbol(t_list **ret, char *str, int idx)
 {
-	
+	int		len;
+
+	return (len);
+}
+
+t_list	*lexer(char *str)
+{
+	t_list	*ret;
+	int		idx;
+
+	ret = NULL;
+	idx = 0;
+	while (str[idx])
+		idx += check_symbol(&ret, str, idx);
+	return (ret);
 }
