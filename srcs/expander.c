3af1ebc52309378288ed94e4ce12e97dc9a316d8
mki <mki@student.42seoul.kr>
2021-05-05 15:34:40 +0900

    parse

--- srcs/expander.c
+++ srcs/expander.c
@@ -6,7 +6,19 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 10:12:43 by mki               #+#    #+#             */
-/*   Updated: 2021/05/03 10:12:45 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/05 14:09:43 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../minishell.h"
+
+// t_word	*word_new(char *str)
+// {
+// 	t_word	*ret;
+
+// 	ret = (t_word *)malloc(sizeof(t_word));
+// 	ret->argv[0] = str;
+// 	ret->fd_in = NULL;
+// 	ret->fd_out = NULL;
+// 	return (ret);
+// }
--- srcs/parser.c
+++ srcs/parser.c
@@ -6,55 +6,39 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 10:12:50 by mki               #+#    #+#             */
-/*   Updated: 2021/05/03 20:44:20 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/05 14:56:23 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minishell.h"
+// #include "../minishell.h"
 
-t_list	*tmp(t_list **word, t_list *token)
-{
+// void	check_token_name(t_list **lst_word, t_token *token)
+// {
+// 	if (token->name = 's')
+// 		ft_lstadd_back(lst_word, ft_lstnew(token->value));
+// 	else if (token->name = ' ')
+// 		;
+// 	else if (token->name = '|')
+// 		;
+// 	else if (token->name = ';')
+// 		;
+// 	else if (token->name = '\"')
+// 		;
+// 	else if (token->name = '\'')
+// 		;
+// }
 
-}
+// t_list	*parser(t_list *lst_token)
+// {
+// 	t_list	*lst_begin;
+// 	t_token	*token;
 
-void	check_token_name(t_list **lst_word, t_token *token)
-{
-	if (token->name = 's')
-		ft_lstadd_back(lst_word, ft_lstnew(token->value));
-	else if (token->name = ' ')
-		;
-	else if (token->name = '|')
-		;
-	else if (token->name = ';')
-		;
-	else if (token->name = '\"')
-		;
-	else if (token->name = '\'')
-		;
-}
-
-t_word	*word_new(char *str)
-{
-	t_word	*ret;
-
-	ret = (t_word *)malloc(sizeof(t_word));
-	ret->argv[0] = str;
-	ret->fd_in = NULL;
-	ret->fd_out = NULL;
-	return (ret);
-}
-
-t_list	*parser(t_list *lst_token)
-{
-	t_list	*lst_word;
-	t_token	*token;
-
-	lst_word = lst_token;
-	while (lst_token)
-	{
-		token = lst_token->content;
-		check_token_name(&lst_word, token);
-		lst_token = lst_token->next;
-	}
-	return (lst_word);
-}
+// 	lst_begin = lst_token;
+// 	while (lst_token)
+// 	{
+// 		token = lst_token->content;
+// 		lst_token = check_syntax(&lst_begin, token);
+// 		lst_token = lst_token->next;
+// 	}
+// 	return (lst_begin);
+// }
