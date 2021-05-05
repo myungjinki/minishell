4b0e0370239811af35b62e8049011b4e376e8a47
mki <mki@student.42seoul.kr>
2021-05-05 17:08:11 +0900

    parsed

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,39 +6,50 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 10:12:50 by mki               #+#    #+#             */
-/*   Updated: 2021/05/05 14:56:23 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/05 16:11:23 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../minishell.h"
+#include "../minishell.h"
 
-// void	check_token_name(t_list **lst_word, t_token *token)
-// {
-// 	if (token->name = 's')
-// 		ft_lstadd_back(lst_word, ft_lstnew(token->value));
-// 	else if (token->name = ' ')
-// 		;
-// 	else if (token->name = '|')
-// 		;
-// 	else if (token->name = ';')
-// 		;
-// 	else if (token->name = '\"')
-// 		;
-// 	else if (token->name = '\'')
-// 		;
-// }
+t_list 	*syntax_quotes(t_list *lst, int token_name)
+{
+	return (lst);
+}
 
-// t_list	*parser(t_list *lst_token)
-// {
-// 	t_list	*lst_begin;
-// 	t_token	*token;
+t_list	*syntax_pipe_semi(t_list *lst, int token_name)
+{
+	return (lst);
+}
 
-// 	lst_begin = lst_token;
-// 	while (lst_token)
-// 	{
-// 		token = lst_token->content;
-// 		lst_token = check_syntax(&lst_begin, token);
-// 		lst_token = lst_token->next;
-// 	}
-// 	return (lst_begin);
-// }
+t_list *syntax_redirection(t_list *lst, int token_name)
+{
+	return (lst);
+}
+
+t_list	*syntax(t_list **lst, t_token *token)
+{
+	if (token->name == '\"' || token->name == '\'')
+		syntax_quotes(*lst, token->name);
+	else if (token->name == ';' || token->name == '|')
+		syntax_pipe_semi(*lst, token->name);
+	else if (token->name == '>' || token->name == '<')
+		syntax_redirection(*lst, token->name);
+	else
+		*lst = (*lst)->next;
+	return (*lst);
+}
+
+t_list	*parser(t_list *lst_token)
+{
+	t_list	*lst_begin;
+	t_token	*token;
+
+	lst_begin = lst_token;
+	while (lst_token)
+	{
+		token = lst_token->content;
+		lst_token = syntax(&lst_begin, token);
+	}
+	return (lst_begin);
+}
