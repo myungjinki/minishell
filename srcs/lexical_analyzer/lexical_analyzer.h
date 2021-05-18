62a8e6aaa0b16cff2905a71c941ed884a786c9fd
mki <mki@student.42seoul.kr>
2021-05-18 16:14:43 +0900

    doing dquote

--- srcs/lexical_analyzer/lexical_analyzer.h
+++ srcs/lexical_analyzer/lexical_analyzer.h
@@ -6,7 +6,7 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 16:31:42 by mki               #+#    #+#             */
-/*   Updated: 2021/05/18 13:28:06 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/18 14:39:19 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ t_list		*lexer(char *str);
 t_list		*lexical_analyzer(char *str, t_envp_list *lst_envp, int status);
 t_list		*make_string(t_list *start, t_list *end);
 int			parser_backslash(t_list *lst_begin, int mode);
-int			parser_dquotes(t_list *lst_begin, t_envp_list *lst_envp);
+int			parser_dquotes(t_list *lst_begin, t_envp_list *lst_envp, int status);
 int			parser_env(t_list *lst_begin, t_envp_list *lst_envp, int status);
 int			parser_pipeline(t_list *lst_begin);
 int		 	parser_quotes(t_list *lst_begin);
--- srcs/lexical_analyzer/parser_dquotes.c
+++ srcs/lexical_analyzer/parser_dquotes.c
@@ -6,23 +6,24 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 15:19:30 by mki               #+#    #+#             */
-/*   Updated: 2021/05/13 15:30:41 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/18 16:13:35 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexical_analyzer.h"
 
-int	parser_dquotes(t_list *lst_begin, t_envp_list *lst_envp)
+int	parser_dquotes(t_list *lst_begin, t_envp_list *lst_envp, int status)
 {
 	lst_begin = 0;
 	lst_envp = 0;
+	status = 0;
 	// t_list	*lst;
 
 	// lst = lst_begin;
 	// while (lst)
 	// {
-	// 	parser_backslash(lst);
-	// 	parser_env(lst, lst_envp);
+	// 	parser_backslash(lst, 1);
+	// 	parser_env(lst, lst_envp, status);
 	// 	lst = lst->next;
 	// }
 	return (0);
