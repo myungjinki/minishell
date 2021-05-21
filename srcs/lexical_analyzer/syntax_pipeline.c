257a73f9be2de43356abdf65d424c3c46ea59532
mki <mki@student.42seoul.kr>
2021-05-21 13:37:46 +0900

    doing syntax_pipeline.c

--- srcs/lexical_analyzer/syntax_pipeline.c
+++ srcs/lexical_analyzer/syntax_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 12:51:01 by mki               #+#    #+#             */
-/*   Updated: 2021/05/20 12:53:10 by mki              ###   ########.fr       */
+/*   Updated: 2021/05/21 13:37:20 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 int	syntax_pipeline(t_list *lst_begin)
 {
+	lst_begin = 0;
+	// t_list	*lst;
+	// t_token	*token;
 
+	// lst = lst_begin;
+	// token = lst->content;
+	// if (token->name == '|')
+	// {
+	// 	lst = lst->next;
+	// 	if (lst)
+	// 	{
+
+	// 	}
+	// 	else
+	// 		return (syntax_error(PIPELINE_SYNTAX))
+	// }
+	// while (lst)
+	// {
+		
+	// }
+	return (0);
 }
