63f954837c98c755a55177d6e4976df1b1ec73bd
mki <mki@student.42seoul.kr>
2021-06-09 13:37:33 +0900

    parser

--- srcs/lexical_analyzer/make_word.c
+++ srcs/lexical_analyzer/make_word.c
@@ -6,7 +6,7 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 12:54:15 by mki               #+#    #+#             */
-/*   Updated: 2021/06/09 13:29:12 by mki              ###   ########.fr       */
+/*   Updated: 2021/06/09 13:37:18 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ t_list	*make_word(t_list *lst_begin)
 	t_list	*ret;
 	
 	print_lst_token(lst_begin);
-		// if (parser_redirection(ret))
-	// 	return (NULL);
+	
 	// lst_token_free(ret, NULL);
 	// print_lst_word(ret);
 	return (NULL);
