c24f2ea8657315566b78a5dcadc4a2b83b4c1be7
mki <mki@student.42seoul.kr>
2021-06-15 17:50:44 +0900

    lex

--- lexical_analyzer/lexical_analyzer.c
+++ lexical_analyzer/lexical_analyzer.c
@@ -6,7 +6,7 @@
 /*   By: mki <mki@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 12:44:16 by mki               #+#    #+#             */
-/*   Updated: 2021/06/15 16:02:43 by mki              ###   ########.fr       */
+/*   Updated: 2021/06/15 17:18:13 by mki              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,29 @@ void	print_lst_token(t_list *lst)
 
 t_list	*lexical_analyzer(char *str, t_envp_list *lst_envp, int status)
 {
+	t_list	*lst_word;
 	t_list	*lst;
 	char	*trim;
 
 	trim = ft_strtrim(str, " ");
 	if (syntax_multline(trim))
+	{
+		free(trim);
 		return (NULL);
+	}
 	if (!(lst = lexer(trim)))
 		return (NULL);
 	free(trim);
 	if (parser(lst, lst_envp, status))
+	{
+		lst_token_free(lst, NULL);
 		return (NULL);
-	lst = make_word_list(lst, make_word(lst));
-	if (!lst)
+	}
+	system("leaks minishell");
+	lst_word = make_word_list(lst, make_word(lst));
+	system("leaks minishell");
+	lst_token_free(lst, NULL);
+	if (!lst_word)
 		return (NULL);
-	return (lst);
+	return (lst_word);
 }
