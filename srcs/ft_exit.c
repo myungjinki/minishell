c0f9e9022cc629af9ad99f827fb0097b892a6178
Seungwan Han <sehan@c3r5s1.42seoul.kr>
2021-06-16 12:36:48 +0900

    final

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,28 +6,39 @@
 /*   By: sehan <sehan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 14:31:13 by sehan             #+#    #+#             */
-/*   Updated: 2021/06/15 22:03:01 by sehan            ###   ########.fr       */
+/*   Updated: 2021/06/16 11:57:10 by sehan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_exit(t_mini *mini, char **temp)
+static void	exit_free(t_mini *mini, int i)
 {
 	ft_d_lstclear(&mini->head);
 	ft_envp_lstclear(&mini->env);
 	free(mini->str);
+	exit(i);
+}
+
+void		ft_exit(t_mini *mini, char **temp)
+{
 	if (temp && temp[1])
 	{
-		if (temp[1][0] < '0' || temp[1][0] > '9')
+		if (temp[2])
+		{
+			mini->status = 1;
+			ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		}
+		else if (temp[1][0] < '0' || temp[1][0] > '9')
 		{
 			ft_putstr_fd("minishell: exit: ", 2);
 			ft_putstr_fd(temp[1], 2);
 			ft_putstr_fd(": numeric argument required\n", 2);
-			exit(255);
+			exit_free(mini, 255);
 		}
-		exit(ft_atoi(temp[1]));
+		else
+			exit_free(mini, ft_atoi(temp[1]));
 	}
 	else
-		exit(0);
+		exit_free(mini, 0);
 }
